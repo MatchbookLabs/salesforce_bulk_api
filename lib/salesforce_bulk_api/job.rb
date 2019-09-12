a8dfb1cf3cbf911a61f7585b82b565afc03215e9
Lawrence Shah <lawrence@getfeedback.com>
2019-09-11 17:06:53 -0700

    add comment

--- lib/salesforce_bulk_api/job.rb
+++ lib/salesforce_bulk_api/job.rb
@@ -173,6 +173,7 @@ module SalesforceBulkApi
             if job_status && job_status['state'] && job_status['state'][0] == 'Closed'
               batch_statuses = {}
 
+              # ignore nils for now, since alternative is to pointlessly make API calls that never terminate
               batches_ready = @batch_ids.compact!.all? do |batch_id|
                 batch_state = batch_statuses[batch_id] = self.check_batch_status(batch_id)
                 batch_state && batch_state['state'] && batch_state['state'][0] && !['Queued', 'InProgress'].include?(batch_state['state'][0])
